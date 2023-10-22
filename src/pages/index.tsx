dcc47ed769bf70becd2e6963e744565581c0afd5
giseliesteticista <140640560+giseliesteticista@users.noreply.github.com>
2023-10-22 12:06:39 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -171,8 +171,159 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
-      <h1>SISTEMA EM MANUTENÇÃO! VOLTAMOS EM BREVE!</h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>AGENDAMENTOS</h1>
+        <p className={styles.desc}>Faça seu agendamento de forma simples, rápida e em poucos passos.</p>
+
+        <div className={styles.appointmentForm}>
+          <input 
+            type='text' 
+            placeholder='Nome' 
+            value={nome} 
+            onChange={txt => setNome(txt.target.value)} 
+          />
+
+          <input 
+          type='tel' 
+          placeholder='Telefone' 
+          minLength={8} 
+          maxLength={11} 
+          value={telefone} 
+          onChange={txt => setTelefone(txt.target.value)}
+          />
+
+          <select 
+          className={styles.servicesSelector}
+          defaultValue=''
+          value={service}
+          onChange={(e) => setService(e.target.value)}
+          >
+            <option value='' disabled>Clique para selecionar um serviço:</option>
+            <option value='designSimples'>Design Simples</option>
+            <option value='designHenna'>Design Henna</option>
+            <option value='micropigmentacao'>Micropigmentação</option>
+            <option value='retoque'>Retoque de Micropigmentação</option>
+          </select>
+
+          <label htmlFor="data">Data e horário do agendamento:</label>
+          <DatePicker
+            selected={data}
+            onChange={(date) => {
+              setData(date);
+              getFreeAppointments(date);
+            }}
+            filterDate={isWeekday}
+            placeholderText={data}
+            dateFormat="dd/MM/yyyy"
+            minDate={new Date()}
+            maxDate={moment().add(62, 'days').toDate()}
+            locale="pt-BR"
+            />
+
+          <div className={styles.freeHours} id="freehours">
+            {
+              avaiableHours.length > 0 ?
+              avaiableHours.map((hrs, i) => {
+                return <button id={`${hrs}`} key={i} onClick={() => {
+                  setPrevHour(hour);
+                  setHour(hrs);
+                  document.getElementById(hrs).style.backgroundColor = 'green';
+                }}
+                >{hrs}</button>
+              })
+              : hourLoading ? <h3 style={{ color: 'black', font: '500 1.1.rem Red Hat Display, sans-serif' }}>CARREGANDO HORÁRIOS DISPONÍVEIS</h3> : <h3 style={{ color: 'black', font: '500 1.1.rem Red Hat Display, sans-serif' }}>SEM HORÁRIOS DISPONÍVEIS! ESCOLHA OUTRA DATA.</h3>
+            }
+          </div>
+        
+          {
+            !loading ?
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <button 
+            type="submit" 
+            className={styles.submitButton}
+            onClick={toAppointment}
+            >
+              AGENDAR
+            </button>
+            <button 
+            type="submit" 
+            className={styles.submitButton}
+            onClick={() => router.push('/agendamentos')}
+            >
+              VER AGENDAMENTOS
+            </button>
+          </div>
+          :
+          <h3 style={{ color: 'black', font: '500 1.1.rem Red Hat Display, sans-serif' }}>CARREGANDO...</h3> 
+          }
+          <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={modalHandle}
+          contentLabel="Sucesso!"
+          style={{
+            content: {
+              width: '80%',
+              top: '50%',
+              left: '50%',
+              right: 'auto',
+              bottom: 'auto',
+              marginRight: '-50%',
+              transform: 'translate(-50%, -50%)',
+            }
+          }}
+          >
+              {
+                appointSuccess ? 
+                <div>
+                  <h3 style={{
+                    font: `900 1.6rem Red Hat Display, sans-serif`,
+                    color: 'black',
+                    textAlign: 'center',
+                    marginTop: 20,
+                  }}>Agendamento realizado com sucesso!</h3>
+                  <div style={{ padding: '40px' }}>
+                    <p style={{ font: `400 1rem Red Hat Display, sans-serif`, color: 'black', marginBottom: '12px'}}>Nome: {`${nome}`}</p>
+                    <p style={{ font: `400 1rem Red Hat Display, sans-serif`, color: 'black', marginBottom: '12px' }}>Telefone: {`${telefone}`}</p>
+                    <p style={{ font: `400 1rem Red Hat Display, sans-serif`, color: 'black', marginBottom: '12px' }}>Data: {`${moment(data).format('DD/MM/YYYY')}`}</p>
+                    <p style={{ font: `400 1rem Red Hat Display, sans-serif`, color: 'black', marginBottom: '12px' }}>Horário: {`${hour}`}</p>
+                  </div>
+                </div>
+                : 
+                <div>
+                <h3 style={{
+                  font: `900 1.6rem Red Hat Display, sans-serif`,
+                  color: 'black',
+                  textAlign: 'center',
+                  marginTop: 20,
+                }}>Agendamento não realizado!</h3>
+                <div style={{ padding: '40px' }}>
+                    <p style={{ font: `400 1rem Red Hat Display, sans-serif`, color: 'black', marginBottom: '12px'}}>Não foi possível completar seu agendamento, por favor, recarregue a página e tente novamente. Se o erro persistir, contate Giseli.</p>
+                </div>
+              </div>
+              }
+              <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                width: '100%'
+              }}
+              >
+                <button 
+                style={{
+                  cursor: 'pointer',
+                  padding: '11px 22px',
+                  backgroundColor: '#bb9f4e',
+                  border: '1px solid #bb9f4e',
+                  color: 'white',
+                  font: `900 1rem Red Hat Display, sans-serif`,
+                }}
+                onClick={modalHandle}
+                >
+                  OK
+                </button>
+              </div>
+          </Modal>
+        </div>
       </main>
       <footer>
         <p style={{ textAlign: 'center', font: '500 1rem Red Hat Display, sans-serif'}}>Desenvolvido por <Link href="https://instagram.com/leonimeloo" target="_blank">@leonimeloo</Link></p>
